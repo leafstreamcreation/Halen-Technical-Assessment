5f810d608fa5bc3ab365939c9325940d649a1246
Derek <shouldergiant@icloud.com>
2021-07-12 12:38:03 -0400

    basic signup/login logic

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,11 +20,71 @@ const saltRounds = 10;
 
 
 router.post("/signup", (req, res, next) => {
-    res.status(200).send("Boop!");
+    const { username, password, email, phonenumber } = req.body;
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    
+    if (!username || !password || !email || !phonenumber) {
+      return res
+        .status(400)
+        .send("Please provide your username, password, email, and phone number");
+    }
+    else if (!passwordRegex.test(password)) {
+        return res.status(400).send(
+            "Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter",
+        );
+    }
+    else User.findOne({ username }).then((found) => {
+        if (found) return res.status(400).send(`User "${username}" already exists`);
+        else return bcrypt
+        .genSalt(saltRounds)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hashedPassword) => {
+            return User.create({
+                username,
+                passhash: hashedPassword,
+                email,
+                phonenumber,
+            });
+        })
+        .then((user) => res.status(201).json({ 
+            userId: user._id, 
+            username: user.username, 
+            password: password, 
+            email: user.email, 
+            phonenumber: user.phonenumber
+        }))
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) return res.status(400).send(`${error.message}`);
+            else return res.status(500).send(`${error.message}`);
+        });
+    });
 });
 
 router.post("/login", (req, res, next) => {
+    const { username, password } = req.body;
+    if (!username) return res
+        .status(400)
+        .send("Please provide your username."); 
+    else if (!password) return res
+        .status(400)
+        .send("Please provide your password");
 
+    User.findOne({ username })
+    .then((user) => {
+        if (!user) return res.status(400).send(`User "${username}" not found`);
+    
+        bcrypt.compare(password, user.passhash).then((isSamePassword) => {
+            if (!isSamePassword) return res.status(400).send("Wrong password");
+            else Session.create({ user: user._id, createdAt: Date.now() })
+            .then( (session) => {
+                console.log(session);
+                return res.json({ token: session._id, createdAt: session.createdAt });
+            });
+        });
+    })
+    .catch((err) => {
+        next(err);
+    });
 });
 
 
@@ -32,9 +92,8 @@ router.post("/sms", (req, res, next) => {
     sms77Request.post("/sms", {}, { params: {
         to: "13032632271",
         text: "I HAVE THE PANTS",
-        from: "sms77.io",
     }}).then(response => {
-        console.log(response)
+        console.log(response);
         res.status(200).json(response.data);
     }).catch(error => {
         console.log(error);
