cc6a0d8bc8a37480794c2182931089f18d199be0
Derek <shouldergiant@icloud.com>
2021-07-13 15:01:02 -0400

    modified homepage message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const authRoutes = require("./auth.routes");
 
 router.get("/", (req, res, next) => {
-    res.status(200).send("Contact");
+    res.status(200).send("Halen Technical Assessment: User login and confirmation backend is live!");
 });
 
 router.use("/auth", authRoutes);
